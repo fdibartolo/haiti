455693d057e20068971c6cb78d8702789382ee7a
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2017-12-03 11:26:12 -0300

    set log level

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # GPIO.setmode(GPIO.BCM)
 GPIO.setmode(GPIO.BOARD)
-logging.basicConfig(filename='log/haiti.log', format='%(asctime)s %(levelname)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.basicConfig(level=logging.DEBUG, filename='log/haiti.log', format='%(asctime)s %(levelname)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
 
 @app.route("/")
